f4b2e809069e65eb3261116ab0dd0962ced8f05a
HeleWho <99724157+HeleWho@users.noreply.github.com>
2022-12-01 21:33:48 +0100

    Create Class_Game.py

---
+++ Class_Game.py
@@ -0,0 +1,23 @@
+class Games:
+    def __init__(self,
+                 player:str,
+                 view:
+                 controleur:str):
+        self._player = player
+        self._controleur = controleur
+        return self
+
+    def create_player(self, playername):
+        return self._create_player
+
+    def get_player(self):
+        return Protagonist
+
+    def create_view(self):
+        return View
+
+    def create_controleur(self,controleur,View):
+        return self._controleur
+
+    def get_controleur(self):
+        return Controleur
